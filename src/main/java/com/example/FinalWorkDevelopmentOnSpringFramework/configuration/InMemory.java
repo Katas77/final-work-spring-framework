8e1aecb5a06208c67fce6ae4bdac8c6d9ff2a147
krp77 <Krp77@mail.ru>
2025-02-13 12:39:12 +0300

    120224

--- src/main/java/com/example/FinalWorkDevelopmentOnSpringFramework/configuration/InMemory.java
+++ src/main/java/com/example/FinalWorkDevelopmentOnSpringFramework/configuration/InMemory.java
@@ -7,21 +7,30 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
+
 @Configuration
 @EnableWebSecurity
-@EnableMethodSecurity
-@RequiredArgsConstructor
 @ConditionalOnProperty(prefix = "app.security", name = "type", havingValue = "mr")
 public class InMemory {
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         UserDetails ramesh = (UserDetails) User.builder()
@@ -36,7 +45,18 @@ public class InMemory {
                 .build();
         return new InMemoryUserDetailsManager(ramesh, admin);
     }
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder(12);
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http, AuthenticationManager authenticationManager) throws Exception {
+        http.authorizeHttpRequests((auth) -> auth
+                        .anyRequest().authenticated())
+                .csrf(AbstractHttpConfigurer::disable)
+                .httpBasic(Customizer.withDefaults())
+                .sessionManagement(httpSecuritySessionManagementConfigurer ->
+                        httpSecuritySessionManagementConfigurer.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authenticationManager(authenticationManager);
+        return http.build();
     }
+
+
 }
